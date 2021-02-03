fc9f3a41ba18a7f1baa0bb47fdf82909c3ae1946
Aurore <aurore31140@gmail.com>
2021-02-03 22:21:23 +0100

    bon euh je fais des tests de déplacements de carré x)

---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb2DPlayer;
+
+    [SerializeField] private float speedMovement;
+    [SerializeField] private float speedJump;
+    //[SerializeField] private float climbJump = 200f;
+
+    [SerializeField] private float horizontalMove;
+
+    private bool Jumping;
+    //[SerializeField] private bool Climbing = false;
+    private bool Walking;
+
+    private Vector3 velocity = Vector3.zero;
+
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckRay;
+    [SerializeField] private LayerMask collisionLayer;
+
+    private void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal") * speedMovement * Time.deltaTime;
+
+        if (Input.GetButtonDown("Jump") && Walking == true)
+        {
+            Jumping = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Walking = Physics2D.OverlapCircle(groundCheck.position, groundCheckRay, collisionLayer);
+        MovePlayer(horizontalMove);        
+    }
+
+    void MovePlayer(float _horizontalMovement)
+    {
+        Vector3 targetSpeed = new Vector2(_horizontalMovement, rb2DPlayer.velocity.y);
+        rb2DPlayer.velocity = Vector3.SmoothDamp(rb2DPlayer.velocity, targetSpeed, ref velocity, 0.05f);
+
+        if (Jumping == true)
+        {
+            rb2DPlayer.AddForce(new Vector2(0f, speedJump));
+            Jumping = false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRay);
+    }
+}
