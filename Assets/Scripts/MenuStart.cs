8a117148fdf357fb1cadd0220f05e30d511eebfc
Aurore <aurore31140@gmail.com>
2021-02-03 23:37:43 +0100

    bidouillage du menu start

---
+++ Assets/Scripts/MenuStart.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuStart : MonoBehaviour
+{
+    public void StartButton()
+    {
+        SceneManager.LoadScene("MoveScene");
+    }
+
+    public void OptionsButton()
+    {
+        //mettre un game object qui apparait et disparait (comme ça je peux utiliser le meme script dans le menu pause)
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
