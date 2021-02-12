c4d9d178390d7169d27e2ef355d188ced4f06a21
Aurore <aurore31140@gmail.com>
2021-02-12 11:28:19 +0100

    ON A LE SPLASH SCREEN

---
+++ Assets/Scripts/VideoScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class VideoScript : MonoBehaviour
+{
+    [SerializeField] private VideoPlayer m_videoPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_videoPlayer.loopPointReached += VideoOver;
+    }
+
+    void VideoOver(UnityEngine.Video.VideoPlayer vp)
+    {
+        //vp.Stop();
+        SceneManager.LoadScene("StartScene");
+    }
+}
