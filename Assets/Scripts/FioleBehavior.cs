b481f23134768995128cd08fcb7f00d7106a7222
Aurore <aurore31140@gmail.com>
2021-03-16 13:51:01 +0100

    je modifie

---
+++ Assets/Scripts/FioleBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FioleBehavior : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D fiole;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
