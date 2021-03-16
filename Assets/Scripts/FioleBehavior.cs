7b36825273e6af162e638caa4f937d4d670b67f7
Aurore <aurore31140@gmail.com>
2021-03-16 14:24:35 +0100

    gameplay level 1

--- Assets/Scripts/FioleBehavior.cs
+++ Assets/Scripts/FioleBehavior.cs
@@ -6,9 +6,17 @@ public class FioleBehavior : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D fiole;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private DiscussionManager discuss;
+    [SerializeField] private PlayerBehavior player;
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if(collision.gameObject.tag == "Player")
+        {
+            player.animator.SetBool("isRunning", false);
+            player.horizontalMove = 0;
+            Debug.Log("bam on a la couleur");
+            discuss.DiscussionBox.SetActive(true);
+        }
     }
 }
